3cb64381efa72d8b51546825edd16b795d30a493
Guanghua Yang <66936823+Gavinygh@users.noreply.github.com>
2022-07-22 00:31:19 -0700

    Delete assignment3 directory

--- assignment3/client/src/redux/users/actionTypes.js
+++
@@ -1,9 +0,0 @@
-export const actionTypes = {
-    GET_STOREDRECEIPES: 'receipes/getStoredReceipes',
-    GET_DELETEDRECEIPES: 'receipes/getDeletedReceipes',
-    ADD_RECEIPE: 'receipes/addReceipe',
-    DELETE_RECEIPE: 'receipes/deleteReceipe',
-    RESTORE_RECEIPE: 'receipes/restoreReceipe',
-    EDIT_RECEIPE: 'receipes/editReceipe',
-  };
- 
